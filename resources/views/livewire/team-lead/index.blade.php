3eb9c56e356daf1adfdc72f31208f8d372dee141
bilalKhan2-cell <probilal643@gmail.com>
2023-12-15 22:03:37 -0800

    Minor Changes

--- resources/views/livewire/team-lead/index.blade.php
+++ resources/views/livewire/team-lead/index.blade.php
@@ -1,26 +1,26 @@
 <div>
 
-    @livewire('breadcrumbs',[
-        'heading'=>'Dashboard',
-        'subHeading'=>'Team Leaders',
-        'pageTitle'=>'List of Teams With Team Leader Name'
+    @livewire('breadcrumbs', [
+        'heading' => 'Dashboard',
+        'subHeading' => 'Team Leaders',
+        'pageTitle' => 'List of Teams With Team Leader Name',
     ])
 
     <div class="card mb-4">
         <h5 class="card-header">Teams</h5>
         <div class="card-body">
 
-            @if(session()->has('team-success'))
+            @if (session()->has('team-success'))
                 <div class="alert alert-success alert-dismissible" role="alert">
-                    {{session()->get('team-success')}}
-                        <button class="btn-close float-end" data-bs-dismiss="alert">&times;</button>
+                    {{ session()->get('team-success') }}
+                    <button class="btn-close float-end" data-bs-dismiss="alert">&times;</button>
                 </div>
             @endif
 
-            @if(session()->has('team-update'))
+            @if (session()->has('team-update'))
                 <div class="alert alert-primary alert-dismissible" role="alert">
-                    {{session()->get('team-update')}}
-                        <button class="btn-close float-end" data-bs-dismiss="alert">&times;</button>
+                    {{ session()->get('team-update') }}
+                    <button class="btn-close float-end" data-bs-dismiss="alert">&times;</button>
                 </div>
             @endif
 
@@ -37,8 +37,9 @@
                         <label>Select Department: </label>
                         <select wire:model="team_department" class="form-control">
                             <option value="">-- Select Department --</option>
-                            @foreach ($departments as $key=>$value)
-                                <option value="{{$value->id}}">DEPT-{{$value->id}} ({{$value->name}})</option>
+                            @foreach ($departments as $key => $value)
+                                <option value="{{ $value->id }}">DEPT-{{ $value->id }} ({{ $value->name }})
+                                </option>
                             @endforeach
                         </select>
                     </div>
@@ -48,9 +49,10 @@
                     <button class="btn btn-info" wire:click="GetList">Get Data</button>
                 </div>
                 <div class="col-sm-12">
-                        <a href="/team/register" wire:navigate class="btn btn-primary float-end">Register Team</a>
-                        <br><br>
-                    <table id="tblTeams" class="table table-hover table-borderless table-sm table-striped table-reponsive-sm w-100">
+                    <a href="/team/register" wire:navigate class="btn btn-primary float-end">Register Team</a>
+                    <br><br>
+                    <table id="tblTeams"
+                        class="table table-hover table-borderless table-sm table-striped table-reponsive-sm w-100">
                         <thead>
                             <tr>
                                 <th>Team ID</th>
@@ -62,19 +64,21 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @if($teams)
-                            @foreach ($teams as $key=>$value)
-                                <tr>
-                                    <td>TEAM-{{$value->id}}</td>
-                                    <td>{{$value->name}}</td>
-                                    <td>{{$value->email}}</td>
-                                    <td>{{$value->rollno}}</td>
-                                    <td>{{$value->departments->name}}</td>
-                                    <td>
-                                        <a href="/team/{{$value->id}}" wire:navigate class="btn border-0 btn-outline-info rounded-circle"><i class="bx bx-edit"></i></a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                            @if ($teams)
+                                @foreach ($teams as $key => $value)
+                                    <tr>
+                                        <td>TEAM-{{ $value->id }}</td>
+                                        <td>{{ $value->name }}</td>
+                                        <td>{{ $value->email }}</td>
+                                        <td>{{ $value->rollno }}</td>
+                                        <td>{{ $value->departments->name }}</td>
+                                        <td>
+                                            <a href="/team/{{ $value->id }}" wire:navigate
+                                                class="btn border-0 btn-outline-info rounded-circle"><i
+                                                    class="bx bx-edit"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             @endif
                         </tbody>
                     </table>
@@ -85,10 +89,10 @@
 </div>
 
 <script>
-    Livewire.on('initiate-datatable', function () {
+    Livewire.on('initiate-datatable', function() {
         $('#tblTeams').DataTable().destroy();
-        setTimeout(function () {
-            $('#tblTeams').DataTable(); 
+        setTimeout(function() {
+            $('#tblTeams').DataTable();
         }, 500);
     });
 </script>
